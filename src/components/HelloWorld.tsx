ef8b69e83cc8f58c329a1ca1e69dfe2fa698c77b
dipanjanmaity890 <dipanjanmaity890@gmail.com>
2026-05-19 14:30:00 +0530

    chore: add HelloWorld component

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function HelloWorld() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center', color: '#6366f1' }}>
+      <h2>Hello, past! This file was added on May 19, 2026.</h2>
+    </div>
+  );
+}
